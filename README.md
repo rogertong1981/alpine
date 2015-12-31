dd27a24fda799fd642c668eb171826aef6c9997d
rogertong <isuper@qq.com>
2015-12-31 20:46:33 +0800

    修改一些文件描述内容

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alpine
+# 尝试建立一个最精简的php运行环境，基于 alpine linux / nginx / php 搭建
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 echo "========================================="
-echo "===========container starting============"
+echo "=                                       ="
+echo "=          container starting           ="
+echo "=                                       ="
 echo "========================================="
 php-cgi -b 127.0.0.1:9000 -c /etc/php/php.ini &
 nginx -g "daemon off;"
