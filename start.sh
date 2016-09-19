eb398a0df43523bfaa43b3d94c4788a193054015
童彬 <tongbin@lonnte.com>
2016-09-19 10:18:33 +0800

    modify for k8s

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ echo "=          container starting           ="
 echo "=                                       ="
 echo "========================================="
 php-cgi -b 127.0.0.1:9000 -c /etc/php/php.ini &
-nginx -g "daemon off;" &
-/bin/bash
+nginx -g "daemon off;" 
